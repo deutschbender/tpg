b0fb9fcb91535e55c3b4ea1ba9638c160092cbdf
deutschbender <jvanneau@gmail.com>
2018-05-08 21:22:58 +0200

    Add files via upload

---
+++ Basket_Analysis.R
@@ -0,0 +1,38 @@
+library(arules)
+library(arulesViz)
+library(RColorBrewer)
+data("Epub")
+
+data("Groceries")
+inspect(head(Groceries, 3))
+inspect(head(Epub, 3))
+
+a_df3 <- data.frame(
+  TID = c(1,1,2,2,2,3), 
+  item=c("a","b","a","b","c", "b")
+)
+a_df3
+trans4 <- as(split(a_df3[,"item"], a_df3[,"TID"]), "transactions")
+trans4
+inspect(trans4)
+data(iris)
+
+irisDisc <- discretizeDF(iris)
+head(irisDisc)
+rules <- apriori(Groceries, parameter = list(supp = 0.001, conf = 0.80))
+
+arules::itemFrequencyPlot(Groceries,
+                          topN=20,
+                          col=brewer.pal(8,'Pastel2'),
+                          main='Relative Item Frequency Plot',
+                          type="relative",ylab="Item Frequency (Relative)")
+
+plot(rules[1:20],
+     method = "graph",
+     control = list(type = "items"))
+
+
+plot(rules[1:20],
+     method = "scatterplot",
+     engine = "htmlwidget")
+arulesViz::plotly_arules(rules)
